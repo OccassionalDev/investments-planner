22962cf930b41d1f3725beab87be6b5048728fd7
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-14 23:07:23 -0400

    created users in seed file to seed in

--- investments-planner-backend/db/seeds.rb
+++ investments-planner-backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Users
+user_a = User.create(name: 'John Smith', email: 'js@js.com', password: 'js123', password_confirmation: 'js123')
+user_b = User.create(name: 'Jane Doe', email: 'jd@jd.com', password: 'jd123', password_confirmation: 'jd123')
+user_c = User.create(name: 'Demo', email: 'demo@demo.com', password: 'demo123', password_confirmation: 'demo123')
