f2e6d63a7274d43eb977df9732c6c193bbfc9246
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-14 22:54:04 -0400

    added a User through the resource generator and added in has_secure_password and has_secure_password for a recovery password to validate matching passwords along with validations to validate the presence of name and email while also validating the uniqueness of a email

---
+++ investments-planner-backend/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_secure_password
+    has_secure_password :recovery_password, validations: false
+
+    validates_presence_of :name, :email
+    validates_uniqueness_of :email
+end
