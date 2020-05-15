e666c3f425e085ab9bdf33f05869d9ac6000ed2e
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-15 00:26:47 -0400

    migrated migrations for  type and industries and created types and industries in the seed file to add to the database

--- investments-planner-backend/db/schema.rb
+++ investments-planner-backend/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_034643) do
+ActiveRecord::Schema.define(version: 2020_05_15_040356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "industries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "investments", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -24,6 +30,12 @@ ActiveRecord::Schema.define(version: 2020_05_15_034643) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_investments_on_user_id"
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
