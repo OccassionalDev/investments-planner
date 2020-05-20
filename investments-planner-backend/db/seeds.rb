9f2b4f209dcacf5d7a166ccad64c181cd6cdb2a4
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-19 23:25:36 -0400

    added some investments to a account for the seed file

--- investments-planner-backend/db/seeds.rb
+++ investments-planner-backend/db/seeds.rb
@@ -5,3 +5,7 @@ user_c = User.create(name: 'Demo', email: 'demo@demo.com', password: 'demo123', 
 
 # Create Investments
 user_a.investments.create(name: 'Google', invest_type: 'Stock', industry: 'IT Software and Services', shares: 13)
+user_a.investments.create(name: 'Gold', invest_type: 'Commodity', industry: 'Gold', shares: 5)
+user_a.investments.create(name: 'VCSH', invest_type: 'Bond', industry: 'Corporate Bond', shares: 18)
+user_a.investments.create(name: 'Intel', invest_type: 'Stock', industry: 'AI and Robotics', shares: 10)
+user_a.investments.create(name: 'Dunkin Donuts', invest_type: 'Stock', industry: 'Food and Beverage', shares: 15)
