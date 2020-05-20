ce44ff26b3aa88b4ebe62ee428a133e599127687
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-20 00:28:34 -0400

    added a standard url to go with all fetch requests and a static headerConfig to go with the fetch request headers

--- investments-planner-frontend/modules/api-adapter.js
+++ investments-planner-frontend/modules/api-adapter.js
@@ -1,5 +1,11 @@
 class ApiAdapter {
     static homeUrl = 'http://localhost:3000/home';
+    static standardUrl = 'http://localhost:3000';
+
+    static headerConfig = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    };
 }
 
 export default ApiAdapter;
