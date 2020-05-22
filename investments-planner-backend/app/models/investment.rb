0b3e18bb68ce372381edd11923802b42cd7806d8
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-22 00:47:57 -0400

    added a method in the investments model that will give a array of all the industries without repeats

--- investments-planner-backend/app/models/investment.rb
+++ investments-planner-backend/app/models/investment.rb
@@ -2,4 +2,8 @@ class Investment < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :name, :invest_type, :shares, :industry
+
+  def self.industries
+    self.all.map { |investment| investment.industry }.uniq
+  end 
 end
