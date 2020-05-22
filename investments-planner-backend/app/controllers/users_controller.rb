f8b2b571e509a941d0b0441191a584907ad608cb
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-21 21:43:29 -0400

    added a sessions controller and added user_params in users controller along with a create method in both users and sessions controller in the backend

--- investments-planner-backend/app/controllers/users_controller.rb
+++ investments-planner-backend/app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def create
+    end 
+
+    private 
+
+    def user_params(*args)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end 
 end
