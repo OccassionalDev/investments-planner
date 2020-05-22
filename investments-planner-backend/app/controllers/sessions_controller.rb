5aa528bdbb3d8fe3f76d6b14b065648fa509a931
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-21 21:52:20 -0400

    added functionality to users and sessions controller to allow for login and signup in the backend

--- investments-planner-backend/app/controllers/sessions_controller.rb
+++ investments-planner-backend/app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
     def create
+        user = User.find_by_email(params[:user][:email])
+
+        if user && user.authenticate(params[:user][:password])
+            render json: user 
+        else 
+            render json: { error: "Invalid email or password" }
+        end 
     end 
 
     def destroy
--- investments-planner-backend/app/controllers/users_controller.rb
+++ investments-planner-backend/app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
     def create
+        user = User.create(user_params)
+
+        if user.save 
+            render json: user
+        else 
+            render json: { error: user.errors.full_messages }
+        end 
     end 
 
     private 
