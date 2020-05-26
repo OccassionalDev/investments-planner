ed7522139e4a81b240f1446c649873a5d017a87e
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-26 16:57:31 -0400

    finished signupForm function in Users class

--- investments-planner-frontend/modules/users.js
+++ investments-planner-frontend/modules/users.js
@@ -1,12 +1,31 @@
 class Users {
     // Form Creation
     static signUpForm() {
+        return `
+        <div id="signup-form>
+            <form action="POST">
+                <label for="name">Name</label>
+                <input type="text" name="name" required>
 
+                <label for="email">Email</label>
+                <input type="email" name="email" required>
+
+                <label for="password">Password</label>
+                <input type="password" name="password" required>
+
+                <label for="password_confirmation">Confirm Password</label>
+                <input type="password" name="password_confirmation" required>
+
+                <input type="submit" value="Sign Up">
+            </form>
+        </div>
+        `
     }
 
     static loginForm() {
-        
+
     }
+
     // Requests
 }
 
