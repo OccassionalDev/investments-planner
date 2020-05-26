a25977fcc82c2268de0b8fb96210e7cf64ddd1b2
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-26 16:32:41 -0400

    deleted adapters and components and removed the instance of app from index.js to start the js all over again

--- investments-planner-frontend/src/adapters/InvestmentsAdapter.js
+++
@@ -1,9 +0,0 @@
-class InvestmentsAdapter {
-    constructor() {
-        this.baseUrl = 'http://localhost:3000/investments';
-    }
-
-    getInvestments() {
-        return fetch(this.baseUrl).then(res => res.json())
-    }
-}
--- investments-planner-frontend/src/adapters/UsersAdapter.js
+++
@@ -1,5 +0,0 @@
-class UsersAdapter {
-    constructor() {
-        this.baseUrl = 'http://localhost:3000/users';
-    }
-}
