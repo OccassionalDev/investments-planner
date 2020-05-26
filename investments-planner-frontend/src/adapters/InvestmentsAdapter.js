f2243ca6075e79ca747581ea12a20dcd52bf52f1
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-26 16:26:37 -0400

    added app.js to a new folder called components and made a getInvestments function in investmnets adapter

--- investments-planner-frontend/src/adapters/InvestmentsAdapter.js
+++ investments-planner-frontend/src/adapters/InvestmentsAdapter.js
@@ -2,4 +2,8 @@ class InvestmentsAdapter {
     constructor() {
         this.baseUrl = 'http://localhost:3000/investments';
     }
+
+    getInvestments() {
+        return fetch(this.baseUrl).then(res => res.json())
+    }
 }
