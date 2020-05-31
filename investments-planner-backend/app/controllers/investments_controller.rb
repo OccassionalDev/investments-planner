67f480b7cef39277a5df9151419b332221644f78
OccassionalDev <ericaschimmel01@gmail.com>
2020-05-31 19:17:48 -0400

    edited the investments controller to not repeat code and modified the routes to include nested routes for investments in the backend

--- investments-planner-backend/app/controllers/investments_controller.rb
+++ investments-planner-backend/app/controllers/investments_controller.rb
@@ -1,34 +1,39 @@
-class InvestmentsController < ApplicationController    
+class InvestmentsController < ApplicationController  
+    before_action :find_user, only: [:create]
+    before_action :find_user_and_investment, only: [:update, :destroy]
+    
     def create
-        user = User.find_by(id: params[:user_id])
-        investment = user.investments.create(investment_params)
+        investment = @user.investments.create(investment_params)
 
         render json: {
-            user: user,
+            user: @user,
             new_investment: investment
         }
     end 
 
     def update
-        user = User.find_by(id: params[:user_id])
-        investment = user.investments.find_by(id: params[:investment_id])
-
-        if investment.update(investment_params)
-            render json: user
+        if @investment.update(investment_params)
+            render json: @user, include: ["investments"]
         else 
-            render json: { error: investment.errors.full_messages }
+            render json: { error: @investment.errors.full_messages }
         end 
     end 
 
     def destroy
-        user = User.find_by(id: params[:user_id])
-        investment = user.investments.find_by(id: params[:investment_id])
-        investment.destroy
-
-        render json: user, include: ["investments"]
+        @investment.destroy
+        render json: @user, include: ["investments"]
     end 
 
     private 
+
+    def find_user 
+        @user = User.find_by(id: params[:user_id])
+    end 
+
+    def find_user_and_investment
+        find_user
+        @investment = @user.investments.find_by(id: params[:id])
+    end 
 
     def investment_params(*args) 
         params.require(:investment).permit(:name, :invest_type, :industry, :shares)
--- investments-planner-backend/config/routes.rb
+++ investments-planner-backend/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :sessions
-  resources :investments
+  resources :users, only: [:index, :create, :show] do 
+    resources :investments, only: [:create, :update, :destroy]
+  end 
+
+  resources :sessions, only: [:create, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
