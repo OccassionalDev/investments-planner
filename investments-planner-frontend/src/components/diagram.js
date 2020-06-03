904622c4b4ef71046638e00133492a0defe96f33
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-03 17:47:06 -0400

    diagram.js will now return a chart

--- investments-planner-frontend/src/components/diagram.js
+++ investments-planner-frontend/src/components/diagram.js
@@ -1,3 +1,23 @@
 class Diagram {
+    getChart(userData) {
+        let investmentDoughnutChart = new CharacterData(ctx, {
+            type: 'doughnut',
+            data: {
+                datasets: userData.industry_shares,
+                labels: userData.industries
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Your Portfolio'
+                },
 
+                legend: {
+                    position: 'right'
+                }
+            }
+        })
+
+        return investmentDoughnutChart
+    }
 }
